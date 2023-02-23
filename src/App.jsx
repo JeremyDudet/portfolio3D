f539422aea0d19badba892387672d2cac3fc188a
Jeremy Dudet <j.dudet@gmail.com>
2023-02-23 15:23:07 -0800

    import screenshot syntax

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Experience from './components/Experience';
 
 function App() {
   return (
-    <div className="h-[100vh]">
+    <div className="h-[100vh] bg-black">
       <Experience />
     </div>
   );
